04a20b379ab4e8634d42e6b854f4eb49ca949941
monti-monti <62000751+monti-monti@users.noreply.github.com>
2021-12-11 10:44:24 +0800

    pull commit

--- src/main/gitdemo.java
+++ src/main/gitdemo.java
@@ -9,5 +9,6 @@ public class gitdemo {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
